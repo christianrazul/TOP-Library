1403f4fd01ffc44dfe2c78f87bd90e715b21ccb9
christianrazul <razulchristian@gmail.com>
2023-08-13 11:18:28 +0800

    added Book constructor etc

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, numOfPages, hasRead) {
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.hasRead = hasRead;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let numOfPages = document.querySelector("#numOfPages").value;
+  let hasRead = document.querySelector("#hasRead").value;
 }
 
 const newBookBtn = document.querySelector("#new-book-btn");
 newBookBtn.addEventListener("click", (e) => {
   let newBookForm = document.querySelector("#new-book-form");
+  // overwrites style property "display" to "block" so that it shows up in the browser.
   newBookForm.style.display = "block";
+});
+
+const newBookForm = document.querySelector("#new-book-form");
+newBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 });
